22d4ddf987a0c6d5cbfcdb3f88f5e8143251309e
pisya popa <reilighost@gmail.com>
2023-08-19 18:40:21 +0200

    Add exel data system
    Nothin special, semi-update.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
+import os
+import random
+import time
+import csv
 import requests
 from web3 import Web3, HTTPProvider
+from openpyxl import Workbook, load_workbook
 import json
-import time
-import random
-import csv
 
-# Configuration
-MIN_TRANSACTION_DELAY = 30                 # Minimum random delay between transactions
-MAX_TRANSACTION_DELAY = 100                # Maximum random delay between transactions
-MIN_ACCOUNT_DELAY = 600                    # Minimum delay between different wallets
-MAX_ACCOUNT_DELAY = 900                    # Maximum delay between different wallets
-MIN_REPEAT_MINTS = 3                       # Minimum number of mint repetitions for an account
-MAX_REPEAT_MINTS = 9                       # Maximum number of mint repetitions for an account
-USE_PROXIES = True                         # Use proxies or not (For CIS countries, proxies are mandatory, otherwise RPC block. You can use 1 for all accounts)
-SHUFFLE_ACCOUNTS = True                    # Shuffle accounts or not
+# 1. Конфигурационные параметры
+MIN_TRANSACTION_DELAY = 30
+MAX_TRANSACTION_DELAY = 100
+MIN_ACCOUNT_DELAY = 600
+MAX_ACCOUNT_DELAY = 900
+MIN_TARGET_TXNS = 5
+MAX_TARGET_TXNS = 15
+USE_PROXIES = True
+SHUFFLE_ACCOUNTS = True
+EXCEL_PATH = "data.xlsx"
 
-# Each account will take a random NFT from this contract. Do not modify.
 CONTRACTS_AND_QUANTITIES = [
     {"address": "0x4de73D198598C3B4942E95657a12cBc399E4aDB5", "quantity": 1},
     {"address": "0x53cb0B849491590CaB2cc44AF8c20e68e21fc36D", "quantity": 3},
@@ -28,84 +30,90 @@ CONTRACTS_AND_QUANTITIES = [
     {"address": "0x266b7E8Df0368Dd4006bE5469DD4EE13EA53d3a4", "quantity": 3},
     {"address": "0xC47ADb3e5dC59FC3B41d92205ABa356830b44a93", "quantity": 2},
     {"address": "0xDcFB6cB9512E50dC54160cB98E5a00B3383F6A53", "quantity": 100}
-]  # kept as it is
+]
 
-# Load the contract ABI
-with open('abi.json', 'r') as f:
-    abi = json.load(f)
+# 2. Вспомогательные функции
+def create_excel_file(file_path, private_keys):
+    wb = Workbook()
+    ws = wb.active
 
-# Load the private keys
-with open('pkey.txt', 'r') as f:
-    private_keys = [line.strip() for line in f]
+    proxies_list = []
+    if USE_PROXIES:
+        with open('proxies.txt', 'r') as f:
+            proxies_list = [line.strip() for line in f.readlines()]
+            proxies_list.extend(proxies_list * (len(private_keys) // len(proxies_list)))
 
-# Shuffle accounts if necessary
+    for idx, (pkey, proxy) in enumerate(zip(private_keys, proxies_list), 2):
+        ws.cell(row=idx, column=1, value=pkey)
+        ws.cell(row=idx, column=2, value=proxy)
+        target_txns = random.randint(MIN_TARGET_TXNS, MAX_TARGET_TXNS)
+        ws.cell(row=idx, column=3, value=target_txns)
+
+    wb.save(file_path)
+
+
+def load_excel_data(file_path, private_keys):
+    if not os.path.exists(file_path):
+        create_excel_file(file_path, private_keys)
+
+    wb = load_workbook(file_path)
+    ws = wb.active
+
+    private_keys = [row[0].value for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=1)]
+    proxies_list = [row[1].value for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=2, max_col=2)]
+
+    return private_keys, proxies_list
+
+
+# 3. Основная логика
+private_keys = []  # Этот список нужно определить или загрузить где-то ранее в коде
+private_keys, proxies_list = load_excel_data(EXCEL_PATH, private_keys)
+
 if SHUFFLE_ACCOUNTS:
-    random.shuffle(private_keys)
-
-# Load proxies list
-if USE_PROXIES:
-    with open('proxies.txt', 'r') as f:
-        proxies_list = [line.strip() for line in f]
-
-# Ensure that the number of proxies matches the number of private keys
-# If there are more private keys than proxies, then duplicates will be created for proxies
-while len(proxies_list) < len(private_keys):
-    proxies_list.extend(proxies_list)
-
-# Associate each private key with a specific proxy
-wallet_proxy_pairs = list(zip(private_keys, proxies_list))
-
+    combined = list(zip(private_keys, proxies_list))
+    random.shuffle(combined)
+    private_keys, proxies_list = zip(*combined)
 
 w3 = Web3(HTTPProvider('https://rpc.zora.energy'))
 
 if not w3.is_connected():
     print("Not connected to RPC")
-    exit()
+    exit(1)
 
 print("Connected to RPC")
 
-# Create a CSV file to store the results
-with open('transactions.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['Wallet', 'Contract Address', 'Transaction Link', 'Status'])
+with open('abi.json', 'r') as f:
+    abi = json.load(f)
 
-# Process each private key
-for index, (private_key, proxy) in enumerate(wallet_proxy_pairs, 1):
-    # Setting up the request session with the associated proxy
+
+for idx, (private_key, proxy) in enumerate(zip(private_keys, proxies_list), 1):
     session = requests.Session()
-    proxy_data = proxy.split('@')
-    credentials = proxy_data[0]
-    ip_port = proxy_data[1]
-    session.proxies = {
-        "http": f"http://{credentials}@{ip_port}",
-        "https": f"http://{credentials}@{ip_port}",
-    }
+    if USE_PROXIES:
+        credentials, ip_port = proxy.split('@')
+        session.proxies = {
+            "http": f"http://{credentials}@{ip_port}",
+            "https": f"http://{credentials}@{ip_port}",
+        }
     w3.provider = HTTPProvider('https://rpc.zora.energy', session=session)
     sender_address = w3.eth.account.from_key(private_key).address
+
     try:
-        # Choose a random contract
-        contract_and_quantity = random.choice(CONTRACTS_AND_QUANTITIES)
-        contract_address = contract_and_quantity["address"]
-        quantity_to_mint = contract_and_quantity["quantity"]
-
-        # Initialize the contract
-        contract = w3.eth.contract(address=contract_address, abi=abi)
-        mint_function = contract.functions.mint(quantity_to_mint)
-        transaction_data = mint_function._encode_transaction_data()
-
-        # Choose a random number of repeat mints
-        repeat_mints = random.randint(MIN_REPEAT_MINTS, MAX_REPEAT_MINTS)
+        contract_data = random.choice(CONTRACTS_AND_QUANTITIES)
+        contract = w3.eth.contract(address=contract_data["address"],
+                                   abi=abi)
+        transaction_data = contract.functions.mint(contract_data["quantity"])._encode_transaction_data()
+        repeat_mints = random.randint(MIN_TARGET_TXNS, MAX_TARGET_TXNS)
         account_delay = random.randint(MIN_ACCOUNT_DELAY, MAX_ACCOUNT_DELAY)
-        for repeat_index in range(repeat_mints):
-            estimated_gas = w3.eth.estimate_gas({
-                'to': contract_address,
+
+        for _ in range(repeat_mints):
+            txn_params = {
+                'to': contract_data["address"],
                 'from': sender_address,
                 'data': transaction_data
-            })
-
-            # Create the transaction object
+            }
+            estimated_gas = w3.eth.estimate_gas(txn_params)
             transaction = {
-                'to': contract_address,
+                'to': contract_data["address"],
                 'value': 0,
                 'gas': estimated_gas,
                 'maxPriorityFeePerGas': int(w3.to_wei('0.0005', 'gwei')),
@@ -114,33 +122,29 @@ for index, (private_key, proxy) in enumerate(wallet_proxy_pairs, 1):
                 'data': transaction_data,
                 'chainId': 7777777
             }
-
-            # Sign and send the transaction
             signed_txn = w3.eth.account.sign_transaction(transaction, private_key)
             txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-            # Wait for the transaction to complete
             receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
 
-            # Check the transaction status and write to the CSV
             status = "Successful" if receipt.status == 1 else "Failed"
             with open('transactions.csv', 'a', newline='') as csvfile:
                 csvwriter = csv.writer(csvfile)
-                csvwriter.writerow(
-                    [sender_address, contract_address, f"https://explorer.zora.energy/tx/{txn_hash.hex()}", status])
+                csvwriter.writerow([
+                    sender_address,
+                    contract_data["address"],
+                    f"https://explorer.zora.energy/tx/{txn_hash.hex()}",
+                    status
+                ])
 
-            # Display the transaction information
-            checksum_address = w3.to_checksum_address(sender_address)
-            print(f"Wallet: {checksum_address} [{index}/{len(private_keys)}]")
-            print(f"Repeat counts for this wallet: [{repeat_index + 1}/{repeat_mints}]")
+            print(f"Wallet: {sender_address} [{idx}/{len(private_keys)}]")
             if receipt.status == 1:
                 print(f"Transaction successfully completed: https://explorer.zora.energy/tx/{txn_hash.hex()}")
             else:
                 print(f"Transaction was rejected: https://explorer.zora.energy/tx/{txn_hash.hex()}")
+
             delay = random.randint(MIN_TRANSACTION_DELAY, MAX_TRANSACTION_DELAY)
-            print(f"Delay between transactions: {delay} seconds\n" + '-' * 20)
             time.sleep(delay)
-        print(f"Delay between wallets: {account_delay} seconds\n" + '-' * 20)
+
         time.sleep(account_delay)
 
     except Exception as e:
